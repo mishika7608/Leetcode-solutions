7dc33455d2c995ae37da0b0ba78772047c738fac
mishika7608 <csds23040@glbitm.ac.in>
2025-12-23 14:37:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (44.41%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
+    // DP - Iterative
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     vector<int> memo(n,0);
+    //     for (int i=0;i<n;i++){
+    //         if (i==0 || i==1){memo[i]=nums[i];}
+    //         else{
+    //             int mx=0;
+    //             for (int j=0;j<i-1;j++){mx=max(mx,nums[i]+memo[j]);}
+    //             memo[i]=mx;
+    //         }
+    //     }
+    //     int mx=0;
+    //     for (int j=0;j<n;j++){mx=max(mx,memo[j]);}
+    //     return mx;
+    // }
+    
+    // DP - Recursive
+    int maxLoot(int i,int n,vector<int> &memo, vector<int> &nums){
+        if (i>=n){return 0;}
+        if (memo[i]!=-1){return memo[i];}
+        int rob = nums[i]+maxLoot(i+2,n,memo,nums);
+        int skip = maxLoot(i+1,n,memo,nums);
+        memo[i]=max(rob, skip);
+        return memo[i];
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> memo(n,0);
-        for (int i=0;i<n;i++){
-            if (i==0 || i==1){memo[i]=nums[i];}
-            else{
-                int mx=0;
-                for (int j=0;j<i-1;j++){mx=max(mx,nums[i]+memo[j]);}
-                memo[i]=mx;
-            }
-        }
-        int mx=0;
-        for (int j=0;j<n;j++){mx=max(mx,memo[j]);}
-        return mx;
+        vector<int> memo(n,-1);
+        return maxLoot(0,n,memo,nums);
     }
-    // int maxLoot(vector<int> &nums, int i, vector<int> &memo){
-    //     if (i>=nums.size()){return 0;}
-    //     if (memo[i]!=-1){return memo[i];}
-    //     int rob = nums[i]+maxLoot(nums, i+2,memo);
-    //     int skip = maxLoot(nums, i+1, memo);
-    //     memo[i] = max(skip, rob);
-    //     return memo[i];
-    // }
-    // int rob(vector<int>& nums) {
-    //     vector<int> memo(nums.size(), -1);
-    //     return maxLoot(nums, 0, memo);
-    // }
 };
