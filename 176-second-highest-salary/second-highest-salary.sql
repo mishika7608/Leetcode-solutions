f4ef2438c76e70b816178cdfb498b0f34348f149
mishika7608 <csds23040@glbitm.ac.in>
2026-01-21 21:43:53 +0530

    Time: 300 ms (15.09%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,9 @@
 # Write your MySQL query statement below
 SELECT IFNULL(
     (
-    SELECT MAX(salary) FROM(
-    SELECT salary,
-        DENSE_RANK() OVER(ORDER BY salary DESC) as rnk
-        FROM Employee) e WHERE rnk=2
-), NULL) AS SecondHighestSalary;
+        SELECT MAX(salary) FROM(
+            SELECT salary, 
+            DENSE_RANK() OVER(ORDER BY salary DESC) as rnk FROM Employee
+        )e WHERE rnk=2
+    ), NULL
+) as SecondHighestSalary;
