c7cea56eecd386ec7cfb47478db43ea69c6f1517
mishika7608 <csds23040@glbitm.ac.in>
2026-02-17 22:38:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (41.32%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+        for (int i=0;i<=11;i++){
+            for (int j=0;j<=59;j++){
+                if (__builtin_popcount(i)+__builtin_popcount(j)==turnedOn){
+                    string temp = to_string(i);
+                    temp+=':';
+                    if (j<10){temp+='0';}
+                    temp += to_string(j);
+                    ans.push_back(temp);
+                }
+            }
+        }return ans;
+    }
+};
