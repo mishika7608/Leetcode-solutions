2f402679c166f652730fedf2ab49efd0c744b447
mishika7608 <csds23040@glbitm.ac.in>
2026-03-07 00:45:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (80.54%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int setBit(int n){
-        int cnt=0;
-        while (n != 0){
-            n = (n & n-1);
-            cnt++;
-        }
-        return cnt;
-    }
-
     vector<int> countBits(int n) {
-        vector<int> ans;
-        for (int i=0;i<=n;i++){
-            ans.push_back(setBit(i));
-        }return ans;
+        vector<int> ans(n+1);
+
+        for (int i = 1; i <= n; i++){
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        return ans;
     }
 };
