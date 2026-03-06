8fd3c2b2b1a4ba2620511c5e91b0a6928161af4a
mishika7608 <csds23040@glbitm.ac.in>
2026-03-06 11:44:27 +0530

    Time: 3 ms (70.20%) | Memory: 15.6 MB (63.47%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void genFunc(vector<string> &ans, int oc, int cc, int &n, string s){
+        if (oc==n && cc==n){
+            ans.push_back(s);
+            return ;
+        }
+        if (oc<n){
+            genFunc(ans, oc+1, cc, n, s+'(');
+        }
+        if (cc<oc){
+            genFunc(ans, oc, cc+1, n, s+')');
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        int oc=0, cc=0;
+        vector<string> ans;
+        genFunc(ans,oc,cc,n,"");
+        return ans;
+    }
+};
