8371e1b673704716398a78d28b787a6f87774cef
mishika7608 <csds23040@glbitm.ac.in>
2026-03-24 17:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (89.34%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
-    int calc(int a, int b, string c){
-        if (c=="+"){
-            return a+b; 
-        }
-        else if (c=="-"){
-            return a-b; 
-        }
-        else if (c=="*"){
-            return a*b; 
-        }
-        else{
-            if (a==0 || b==0){return 0;}
-            return a/b; 
-        }return 0;
-    }
     int evalRPN(vector<string>& tokens) {
-        int n = tokens.size();
         stack<int> st;
-        for (int i=0;i<n;i++){
-            if (tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
-                int b=st.top();st.pop();
-                int a=st.top();st.pop();
-                int val=calc(a,b,tokens[i]);
-                st.push(val);
-            }else{
-                st.push(stoi(tokens[i]));
+        for (string t : tokens) {
+            if (t == "+" || t == "-" || t == "*" || t == "/") {
+                int b = st.top(); st.pop();
+                int a = st.top(); st.pop();
+                if (t == "+") st.push(a + b);
+                else if (t == "-") st.push(a - b);
+                else if (t == "*") st.push(a * b);
+                else st.push(a / b);
+            } else {
+                st.push(stoi(t));
             }
         }
         return st.top();
