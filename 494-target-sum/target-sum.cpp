00944134f6bf064d518ab520bed0e3ec0d0d8cea
mishika7608 <csds23040@glbitm.ac.in>
2026-03-26 16:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (71.05%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    // int findTargetSumWays(vector<int>& nums, int target) {
-    //     int sum=0;
-    //     for (int x: nums){sum+=x;}
-
-    //     if (sum<abs(target) || (sum+target)%2!=0) return 0;
-
-    //     int P=(sum+target)/2;
-    //     vector<int> dp(P+1, 0);
-    //     dp[0]=1;
-
-    //     for (int x : nums){
-    //         for (int s=P;s>=x;s--){
-    //             dp[s] += dp[s-x];
-    //         }
-    //     }
-    //     return dp[P];
-    // }
     int findTargetSumWays(vector<int>& nums, int target) {
         int total = 0;
         for (int num : nums){total+=num;}
@@ -30,6 +13,5 @@ public:
             }
         }
         return dp[p];
-
     }
 };
