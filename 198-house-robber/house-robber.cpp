3bb346534f4bb1bc12a37adf7ab82e7e6e357d0a
mishika7608 <csds23040@glbitm.ac.in>
2026-03-26 16:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (68.53%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
-    // DP - Iterative
-    // int rob(vector<int>& nums) {
-    //     int n = nums.size();
-    //     vector<int> memo(n,0);
-    //     for (int i=0;i<n;i++){
-    //         if (i==0 || i==1){memo[i]=nums[i];}
-    //         else{
-    //             int mx=0;
-    //             for (int j=0;j<i-1;j++){mx=max(mx,nums[i]+memo[j]);}
-    //             memo[i]=mx;
-    //         }
-    //     }
-    //     int mx=0;
-    //     for (int j=0;j<n;j++){mx=max(mx,memo[j]);}
-    //     return mx;
-    // }
-    
-    // DP - Recursive
-    int maxLoot(int i,int n,vector<int> &memo, vector<int> &nums){
-        if (i>=n){return 0;}
-        if (memo[i]!=-1){return memo[i];}
-        int rob = nums[i]+maxLoot(i+2,n,memo,nums);
-        int skip = maxLoot(i+1,n,memo,nums);
-        memo[i]=max(rob, skip);
-        return memo[i];
-    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> memo(n,-1);
-        return maxLoot(0,n,memo,nums);
+        int prev2 = 0, prev1 = 0;
+        for (int x : nums) {
+            int curr = max(prev1, prev2 + x);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
