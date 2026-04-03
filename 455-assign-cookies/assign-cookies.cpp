b9e914767b6281b6c14bff2828bb8925a742f279
mishika7608 <csds23040@glbitm.ac.in>
2026-04-03 22:46:14 +0530

    Time: 5 ms (79.64%) | Memory: 44.7 MB (77.86%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    //DP
+    // int findContentChildren(vector<int>& g, vector<int>& s) {
+    //     sort(g.begin(), g.end());
+    //     sort(s.begin(), s.end());
+    //     int n = g.size();
+    //     int m = s.size();
+    //     vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+
+    //     for (int i=1;i<=n;i++){
+    //         for (int j=1;j<=m;j++){
+    //             dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+
+    //             if (s[j-1]>=g[i-1]){
+    //                 dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
+    //             }
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+
+
+    // GREEDY
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
