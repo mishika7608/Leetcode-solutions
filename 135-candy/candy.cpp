415a89f39ac1e9e3b6fe3c3feeb15fc9e5f53a81
mishika7608 <csds23040@glbitm.ac.in>
2026-04-03 22:43:40 +0530

    Time: 4 ms (14.76%) | Memory: 23 MB (29.69%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -5,18 +5,17 @@ public:
         int s=0;
         vector<int> candies(n,1);
         for (int i=1;i<n;i++){
-            if (ratings[i-1] < ratings[i]){
+            if (ratings[i-1]<ratings[i]){
                 candies[i]=candies[i-1]+1;
             }
         }
         for (int i=n-2;i>=0;i--){
-            if (ratings[i+1] < ratings[i]){
+            if (ratings[i]>ratings[i+1]){
                 candies[i]=max(candies[i],candies[i+1]+1);
             }
         }
-        for (int i=0;i<n;i++){
-            s+=candies[i];
-        }
+        for (int i=0;i<n;i++){s+=candies[i];}
         return s;
     }
 };
+                          
