7fa839543a22a4bdf4efa3d3a3443cf57ad6e18c
mishika7608 <csds23040@glbitm.ac.in>
2026-04-03 19:08:15 +0530

    Time: 35 ms (96.71%) | Memory: 93.9 MB (46.63%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-    //     int n =points.size();
-    //     sort(points.begin(), points.end(), [](vector<int> 
-    //     &a, vector<int> &b){if (a[0]==b[0]){return a[1]<b[1];}
-    //     return a[0]<b[0];});
-    //     int c=1;int e=points[0][1];
-    //     for (int i=0;i<n;i++){
-    //         if (points[i][0]>e){
-    //             c++;
-    //             e=points[i][1];
-    //         }
-    //     }
-    //     return c;
-    // }
-    sort(points.begin(), points.end(), [](auto &a, auto &b){
-        return a[1] < b[1];
-    });
-
-    int arrows = 1;
-    int end = points[0][1];
-
-    for (int i = 1; i < points.size(); i++) {
-        if (points[i][0] > end) {
-            arrows++;
-            end = points[i][1];
+        int n =points.size();
+        sort(points.begin(), points.end(), [](vector<int> 
+        &a, vector<int> &b){return a[1]<b[1];});
+        int c=1;int e=points[0][1];
+        for (int i=1;i<n;i++){
+            if (points[i][0]>e){
+                c++;
+                e=points[i][1];
+            }
         }
-    }return arrows;
+        return c;
     }
+    
 };
