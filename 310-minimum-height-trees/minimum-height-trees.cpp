15c43394057392d62a63f3c98311e8d6d69d4e99
mishika7608 <csds23040@glbitm.ac.in>
2026-04-17 14:11:01 +0530

    Time: 61 ms (24.35%) | Memory: 60.9 MB (41.00%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-    // BFS TRIMMING LEAVES
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n == 1) return {0};
+        if (n==1){return {0};}
         vector<vector<int>> adj(n);
-        vector<int> degree(n,0);
-        for(auto &e : edges){
+        vector<int> deg(n,0);
+        for (auto e: edges){
             adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-            degree[e[0]]++;
-            degree[e[1]]++;
+            adj[e[1]].push_back(e[0]);            
+            deg[e[0]]++;            
+            deg[e[1]]++;
         }
         queue<int> q;
-        for(int i=0;i<n;i++)
-            if(degree[i] == 1) q.push(i); // initial leaves
-        int remaining = n;
-        while(remaining > 2){
-            int sz = q.size();
-            remaining -= sz;
-            for(int i=0;i<sz;i++){
-                int leaf = q.front(); q.pop();
-                for(int neighbor : adj[leaf]){
-                    degree[neighbor]--;
-                    if(degree[neighbor] == 1)
-                        q.push(neighbor);
+        for (int i=0;i<n;i++){
+            if (deg[i]==1){q.push(i);}
+        }
+        int rem = n;
+        while (rem>2){
+            int sz= q.size();
+            rem-=sz;
+            for (int i=0;i<sz;i++){
+                int u = q.front();q.pop();
+                for (int v : adj[u]){
+                    deg[v]--;
+                    if (deg[v]==1){q.push(v);}
                 }
             }
         }
         vector<int> res;
-        while(!q.empty()){
-            res.push_back(q.front());
-            q.pop();
+        while (!q.empty()){
+            res.push_back(q.front());q.pop();
         }
         return res;
     }
