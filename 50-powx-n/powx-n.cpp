ffb222ba9457f37598f45cf6ccfd4f68363f49fb
mishika7608 <csds23040@glbitm.ac.in>
2026-05-01 13:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.35%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n==0){return 1.0;}
+        if (n<0){
+            if (n==INT_MIN){
+                return 1/myPow(x,-(n+1))*x;
+            }
+            return 1/myPow(x,-n);
+        }
+        double half = myPow(x,n/2);
+        if (n%2==0){return half*half;}
+        else{return half*half*x;}
+    }
+};
