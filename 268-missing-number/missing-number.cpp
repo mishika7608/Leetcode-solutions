c6505af7cea9f37b42b31edbe8a40e9ad5a97f3f
mishika7608 <csds23040@glbitm.ac.in>
2026-05-02 07:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (37.55%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int xorAll=0;
-        for (int i=0;i<=nums.size();i++){xorAll^=i;}
-        for (int x : nums){xorAll^=x;}
+        int n=nums.size(),xorAll=0;
+        for (int i=0;i<=n;i++){
+            xorAll^=i;
+        }
+        for (int i : nums){
+            xorAll^=i;
+        }
         return xorAll;
     }
 };
