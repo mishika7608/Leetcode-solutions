2c7e9d2a73f9140bcbae46a1fa074d68d07a3ab7
mishika7608 <csds23040@glbitm.ac.in>
2026-05-13 19:08:59 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (82.87%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int end=0;
+        int jumps=0;
         int far=0;
-        int jump=0;
-        int n= nums.size()-1;
-        for (int i=0;i<n;i++){
-            far = max(far,i+nums[i]);
+        int end = 0;
+        for (int i=0;i<nums.size()-1;i++){
+            far=max(far,i+nums[i]);
             if (i==end){
-                jump++;
+                jumps++;
                 end=far;
             }
         }
-        return jump;
+        return jumps;
     }
 };
