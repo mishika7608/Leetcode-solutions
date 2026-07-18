4a889255cedc02f4a0c3bde7b9ebaeb28f81b039
mishika7608 <csds23040@glbitm.ac.in>
2026-07-18 16:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (49.50%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int cnt=__builtin_popcount(num2);
-        int x=0;
-        for (int i=31;i>=0 && cnt>0;i--){
-            if (num1 & (1<<i)){
-                x|=(1<<i);
+        int cnt = __builtin_popcount(num2);
+        int x = 0;
+
+        // Copy highest set bits from num1
+        for (int i = 31; i >= 0 && cnt > 0; i--) {
+            if (num1 & (1 << i)) {
+                x |= (1 << i);
                 cnt--;
             }
         }
-        for (int i=0;i<=31 && cnt>0 ;i++){
-            if (!(x & (1<<i))){
-                x|=(1<<i);cnt--;
+
+        // Fill remaining set bits from lowest positions
+        for (int i = 0; i < 32 && cnt > 0; i++) {
+            if (!(x & (1 << i))) {
+                x |= (1 << i);
+                cnt--;
             }
-        }return x;
+        }
+
+        return x;
     }
 };
