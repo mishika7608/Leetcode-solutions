59d41552f02bb2d304570d7237f2a94da77e7075
mishika7608 <csds23040@glbitm.ac.in>
2026-08-04 08:05:29 +0530

    Time: 5 ms (40.63%) | Memory: 12.1 MB (54.65%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<int>& ans, vector<vector<int>>& res, vector<bool>& used){
-        if(ans.size()==nums.size()){
-            res.push_back(ans);
-            return;
-        }
-
-        for(int i=0;i<nums.size();i++){
-
-            if(used[i]) continue;
-
-            if(i>0 && nums[i]==nums[i-1] && !used[i-1]) continue;
-
+    vector<vector<int>> res;
+    void helper(vector<int> &nums, vector<int> &temp, vector<bool> &used){
+        if (nums.size()==temp.size()){res.push_back(temp);}
+        for (int i=0;i<nums.size();i++){
+            if (used[i]){continue;}
+            if (i>0 && nums[i]==nums[i-1] && !used[i-1]){continue;}
             used[i]=true;
-            ans.push_back(nums[i]);
-
-            helper(nums,ans,res,used);
-
-            ans.pop_back();
-            used[i]=false;
+            temp.push_back(nums[i]);
+            helper(nums,temp,used);
+            temp.pop_back();used[i]=false;
         }
+
     }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-
-        sort(nums.begin(),nums.end());
-
-        vector<vector<int>> res;
-        vector<int> ans;
+        vector<int> temp;sort(nums.begin(),nums.end());
         vector<bool> used(nums.size(),false);
-
-        helper(nums,ans,res,used);
-
+        helper(nums,temp,used);
         return res;
     }
 };
